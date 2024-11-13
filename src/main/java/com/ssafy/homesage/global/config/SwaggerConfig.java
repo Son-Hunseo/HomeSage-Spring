d887e50e4409a446b60a5a70452673aa07106ca2
Lee-HyunSoo <cris_lx@naver.com>
2024-11-13 16:34:40 +0900

    feat: set swagger config

---
+++ src/main/java/com/ssafy/homesage/global/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.ssafy.homesage.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * http://localhost:8080/swagger-ui.html
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API Documentation")
+                        .description("API 명세서입니다.")
+                        .version("v1.0"));
+    }
+}

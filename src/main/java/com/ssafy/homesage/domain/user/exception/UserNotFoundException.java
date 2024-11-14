217599f40a58a14708aa845802d4b965312b1443
Lee-HyunSoo <cris_lx@naver.com>
2024-11-14 17:25:23 +0900

    feat: create Exception Handler, ErrorCode, Exceptions

---
+++ src/main/java/com/ssafy/homesage/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,5 @@
+package com.ssafy.homesage.domain.user.exception;
+
+public class UserNotFoundException extends NullPointerException {
+
+}
---
+++ src/main/java/com/ssafy/homesage/global/error/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.ssafy.homesage.global.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+	
+	BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다."), // 기타 잘못된 요청 처리.
+	DUPLICATED_EMAIL(HttpStatus.CONFLICT.value(), "사용할 수 없는 이메일입니다."),
+	REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED.value(), "비정상 토큰입니다."),
+	USER_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "사용자를 찾을 수 없습니다.");
+	
+	private final int httpStatus;
+	private final String message;
+	
+	ErrorCode(int httpStatus, String message) {
+		this.httpStatus = httpStatus;
+		this.message = message;
+	}
+}

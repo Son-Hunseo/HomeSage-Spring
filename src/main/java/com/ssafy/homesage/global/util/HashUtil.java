64737a5856983dbbd8018736cc8d5503ef363db0
Lee-HyunSoo <cris_lx@naver.com>
2024-11-14 17:27:05 +0900

    feat: convert password with SHA-512

---
+++ src/main/java/com/ssafy/homesage/global/util/HashUtil.java
@@ -0,0 +1,30 @@
+package com.ssafy.homesage.global.util;
+
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * 평문을 SHA-512로 처리하는 클래스.
+ */
+@Component
+public class HashUtil {
+
+	public String getDigest(String plainText) {
+
+		MessageDigest messageDigest = null;
+
+		try {
+			messageDigest = MessageDigest.getInstance("SHA-512");
+			messageDigest.reset();
+			messageDigest.update(plainText.getBytes("UTF-8"));
+		}catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return String.format("%0128x", new BigInteger(1, messageDigest.digest()));
+	}
+}

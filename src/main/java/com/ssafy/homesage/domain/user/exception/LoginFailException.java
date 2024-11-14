5c1eb358b1ee98db6cfca2e4eefbe7ca95e2df46
Lee-HyunSoo <cris_lx@naver.com>
2024-11-14 17:13:11 +0900

    remove: remove file

--- src/main/java/com/ssafy/homesage/domain/user/exception/LoginFailException.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.homesage.domain.user.exception;
-
-public class LoginFailException extends Exception {
-    public LoginFailException() {}
-    public LoginFailException(String message) {
-        super(message);
-    }
-}

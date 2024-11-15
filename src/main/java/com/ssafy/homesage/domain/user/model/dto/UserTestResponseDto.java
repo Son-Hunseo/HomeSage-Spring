7335050f922d3efae717866445f176d350cd90a2
Lee-HyunSoo <cris_lx@naver.com>
2024-11-15 14:18:19 +0900

    fix: update

--- src/main/java/com/ssafy/homesage/domain/user/model/dto/UserTestResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.homesage.domain.user.model.dto;
-
-import com.ssafy.homesage.domain.user.model.entity.User;
-
-public record UserTestResponseDto(String email, String name) {
-    public UserTestResponseDto(User user) {
-        this(user.getEmail(), user.getName());
-    }
-}

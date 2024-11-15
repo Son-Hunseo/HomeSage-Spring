3d31f3042fb07336803d43c3921f90877fc47805
Lee-HyunSoo <cris_lx@naver.com>
2024-11-15 16:25:31 +0900

    feat: if not have interest list, show no content

--- src/main/java/com/ssafy/homesage/global/error/ErrorCode.java
+++ src/main/java/com/ssafy/homesage/global/error/ErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-	
+
+	EMPTY_INTERESTED(HttpStatus.NO_CONTENT.value(), "찜목록이 비어있습니다."),
 	BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다."), // 기타 잘못된 요청 처리.
 	DUPLICATED_EMAIL(HttpStatus.CONFLICT.value(), "사용할 수 없는 이메일입니다."),
 	REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED.value(), "비정상 토큰입니다."),

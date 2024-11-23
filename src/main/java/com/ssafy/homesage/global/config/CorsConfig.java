a0478073be275d64f91fc4e0a61244f0eac73957
Lee-HyunSoo <cris_lx@naver.com>
2024-11-23 16:45:09 +0900

    fix: cookie secure change

--- src/main/java/com/ssafy/homesage/global/config/CorsConfig.java
+++ src/main/java/com/ssafy/homesage/global/config/CorsConfig.java
@@ -14,10 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://" + serverIp + ":5173",
-                        "http://" + serverIp + ":80"
-                )
+                .allowedOrigins("http://" + serverIp + ":5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
